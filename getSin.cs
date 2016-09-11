c65f73ca058839cc7d29cb2579a9526735f070f4
Honza S. Copak <bonum.comunae@volny.cz>
2016-09-11 08:54:16 +0200

    Add files via upload

---
+++ getSin.cs
@@ -0,0 +1,25 @@
+//https://www.raspberrypi.org/forums/viewtopic.php?p=88063
+
+using System;
+
+public class getSinus
+{
+    static public void Main(string[] args)
+        {
+        	//Console.WriteLine ("test arg > "+args[0].ToString()); //ok string
+		double num=Int32.Parse(args[0]); //integer input
+		double sin=Math.Sin(num/180*Math.PI);
+		Console.WriteLine ("sinus >> "+sin.ToString()); //ok int
+        }
+}
+
+
+/*
+compile:
+mcs getSin.cs
+run:
+ ./getSin.exe 30
+return:
+sinus >> 0.5
+*/
+
